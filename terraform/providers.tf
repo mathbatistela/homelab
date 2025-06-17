f0d2f7343c549862726314ce8b28a52aed110f95
MathBatistela <matheus@batistela.tech>
2025-06-16 23:38:50 -0300

    feat: adds cloudflare provider to terraform

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,6 +4,11 @@ terraform {
       source = "Telmate/proxmox"
       version = "3.0.2-rc01"
     }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5.5.0"
+    }
   }
 }
 
@@ -12,4 +17,9 @@ provider "proxmox" {
   pm_user         = var.pm_user
   pm_password     = var.pm_password
   pm_tls_insecure = var.pm_tls_insecure
+}
+
+provider "cloudflare" {
+  email   = var.cloudflare_email
+  api_key = var.cloudflare_api_key
 }

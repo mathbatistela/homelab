4c65f3138c5546968c54910b87dacb4f6c52f951
MathBatistela <matheus@batistela.tech>
2025-08-30 19:34:01 -0300

    feat: adds tools vm and minecraft vm

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,6 +77,7 @@ resource "proxmox_lxc" "database_server" {
   target_node  = var.pve_node
   ostemplate   = local.default_ostemplate
   unprivileged = true
+  nameserver   = "1.1.1.1 8.8.8.8"
 
   cores  = 2
   memory = 4096
@@ -117,7 +118,7 @@ resource "proxmox_lxc" "minecraft_server" {
 
   rootfs {
     storage = var.vm_storage
-    size    = "20G"
+    size    = "50G"
   }
 
   features {
@@ -134,6 +135,79 @@ resource "proxmox_lxc" "minecraft_server" {
   onboot  = true
   startup = "order=1,up=30,down=30"
 
+  ssh_public_keys = file(var.ssh_pub_key_path)
+  password        = var.lxc_root_password
+}
+
+resource "proxmox_lxc" "tools_server" {
+  vmid         = 107
+  hostname     = "tools"
+  target_node  = var.pve_node
+  ostemplate   = local.default_ostemplate
+  unprivileged = true
+
+  cores  = 2
+  memory = 8192
+  swap   = 2048
+
+  rootfs {
+    storage = var.vm_storage
+    size    = "50G"
+  }
+
+  mountpoint {
+    key     = "mp0"
+    slot    = 0
+    volume  = "/mnt/homeshare"
+    mp      = "/mnt/homeshare"
+    backup  = false
+  }
+
+  features {
+    nesting = true
+  }
+
+  network {
+    name   = "eth0"
+    bridge = local.default_network_bridge
+    ip     = "192.168.237.107/24"
+    gw     = "192.168.237.254"
+  }
+
+  onboot  = true
+  startup = "order=1,up=30,down=30"
+  ssh_public_keys = file(var.ssh_pub_key_path)
+  password        = var.lxc_root_password
+}
+
+resource "proxmox_lxc" "tailscale_server" {
+  vmid         = 108
+  hostname     = "tailscale"
+  target_node  = var.pve_node
+  ostemplate   = local.default_ostemplate
+  unprivileged = true
+
+  cores  = 1
+  memory = 512
+
+  rootfs {
+    storage = var.vm_storage
+    size    = "5G"
+  }
+
+  features {
+    nesting = true
+  }
+
+  network {
+    name   = "eth0"
+    bridge = local.default_network_bridge
+    ip     = "192.168.237.108/24"
+    gw     = "192.168.237.254"
+  }
+
+  onboot  = true
+  startup = "order=1,up=30,down=30"
   ssh_public_keys = file(var.ssh_pub_key_path)
   password        = var.lxc_root_password
 }

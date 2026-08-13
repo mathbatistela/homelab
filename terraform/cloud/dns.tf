41623bfd319f1a88c66ee6513cefd629930415aa
Matheus Batistela <mathbatistela@gmail.com>
2026-08-13 18:29:22 -0300

    feat(dns): point casamento-gi-jorge.batistela.tech at Cloud Run

--- terraform/cloud/dns.tf
+++ terraform/cloud/dns.tf
@@ -15,3 +15,12 @@ resource "cloudflare_dns_record" "pangolin_dash" {
   ttl     = 1
   proxied = false
 }
+
+resource "cloudflare_dns_record" "casamento_gi_jorge_cloudrun" {
+  zone_id = var.cloudflare_zone_id
+  name    = "casamento-gi-jorge"
+  type    = "CNAME"
+  content = "ghs.googlehosted.com."
+  ttl     = 300
+  proxied = false
+}
